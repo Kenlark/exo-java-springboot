a04ce483e4e9388634269c7d8d09b7b4d209ee2b
Kenlark <kenzokerachi@hotmail.fr>
2025-05-05 14:24:54 +0200

    initial commit

---
+++ src/main/java/org/example/projetwebspringboot/ProjetWebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.projetwebspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetWebSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetWebSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projetwebspringboot/ProjetWebSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projetwebspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetWebSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
